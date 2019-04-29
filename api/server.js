29d6942abd6075699d25e33fc736059b5135fc66
dgbailey <bailey.dust@gmail.com>
2019-04-28 19:09:46 -0700

    endpoints added for quickbooking and simultaneous registration. endpoints also added for browser extension login and dynamic insertion of subscription details on subscription platforms.

--- api/server.js
+++ api/server.js
@@ -19,6 +19,18 @@ server.get('/', async (req, res) => {
   }
 });
 
+server.get('/users/:id', async (req, res) => {
+  try {
+    const shoutouts = await db('shoutouts');
+    res.status(200).json({
+      username: "dustin",quickbook_subs:[{id:'',sub_details:[{password:'',username:'',expiration:''}]}],network_subs:[{name:'hulu',sub_details:{password:'',username:''}}]});
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  }
+});
+
+
 server.post('/', async (req, res) => {
   try {
     const [id] = await db('shoutouts').insert(req.body);
@@ -31,4 +43,20 @@ server.post('/', async (req, res) => {
   }
 });
 
-module.exports = server;
+
+server.post('/users', async (req, res) => {
+  try {
+    const [id] = await db('shoutouts').insert(req.body);
+
+    //body
+    // {username:'',password:'',quickbook_subs:[{id:'',sub_details:[{password:'',username:'',expiration:''}]}],
+    // 
+
+    const shoutouts = await db('shoutouts');
+
+    res.status(201).json(shoutouts);
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot add the shoutout' });
+  }
+});
